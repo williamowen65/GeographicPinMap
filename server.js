e195ad42f99ce82b01edab801f6361f7c2c82ff5
William Owen <william.owen.bsc@gmail.com>
2024-12-06 13:49:51 -0800

    Refactor server.js to enhance socket connection handling and improve CORS configuration; add comments for clarity

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const socketIo = require('socket.io');
 const path = require('path');
 const cors = require('cors');
 
-  
+// Load environment variables
 require('dotenv').config();
 
 
@@ -13,13 +13,14 @@ const server = http.createServer(app);
 const io = socketIo(server,
     {
         cors: {
+            // handle connection for production server and local dev
             origin: process.env.ENV === 'production' ? "https://notch-marble-garlic.glitch.me/" : "http://localhost:5500",
-            // origin: "localhost:5500",
             methods: ["GET", "POST"]
         }
     }
 );
 
+// 5500 is a special dev port on my computer (mobile dev)
 const PORT = process.env.PORT || 5500;
 
 // Enable CORS
@@ -28,17 +29,22 @@ app.use(cors());
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Keeps track of all locations shared over the server
 let locations = {};
+// Tracks total count of viewers
 let totalViewers = 0;
 
+// Listen for new connections
 io.on('connection', (socket) => {
     console.log('New client connected');
-
+    // Increment total viewers
     totalViewers++;
 
     // Send existing locations to the new client
+    // This uses "Socket" to send data to the new client
     socket.emit('initialLocations', locations);
 
+    // This uses 'io' to send data to all clients
     io.emit('emitViewerCount', totalViewers);
 
     // Listen for new locations from clients
@@ -46,6 +52,7 @@ io.on('connection', (socket) => {
         // attach socketId to location
         location.socketId = socket.id;
         if (locations[socket.id]) {
+            // Update location?
         } else {
             io.emit('broadcastLocation', location);
             locations[socket.id] = location;
@@ -54,8 +61,8 @@ io.on('connection', (socket) => {
         console.log('New location added', { locations })
     });
 
+    // Listen for disconnections
     socket.on('disconnect', () => {
-
         const coordinates = locations[socket.id];
         delete locations[socket.id];
         io.emit('removeLocation', coordinates);
@@ -65,17 +72,15 @@ io.on('connection', (socket) => {
     });
 });
 
+// Start the server
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`URL: http://localhost:${PORT}`);
 });
 
-
+// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
-// Include the socket.io client script
-app.get('/socket.io/socket.io.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'node_modules', 'socket.io', 'client-dist', 'socket.io.js'));
-});
+
